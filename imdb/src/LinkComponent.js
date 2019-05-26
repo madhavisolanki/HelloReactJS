c8fd62edd487ddb76c7339eba277c866cfaceffb
madhavisolanki <maddy9859@gmail.com>
2019-05-26 14:41:44 +0530

    Event handler

--- imdb/src/LinkComponent.js
+++ imdb/src/LinkComponent.js
@@ -2,20 +2,51 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types';
 
 class LinkComponenent extends Component {
+    constructor(props){
+        super(props);
+        this.gotClicked = this.gotClicked.bind(this);
+        this.gotPointer = this.gotPointer.bind(this);
+        this.state = {
+            visited: false
+        }
+    }
+
+    gotClicked(event) {
+        event.preventDefault();
+        console.log("I am an event");
+        console.log(event);
+        console.log(event.target);
+        if(this.state.visited){
+            this.setState({visited: false});
+        }else{
+            this.setState({visited: true});
+        }
+    }
+
+    gotPointer(event){
+        console.log("I am moving pointer");
+        console.log(event);
+        console.log(event.target);
+    }
+    componentDidMount() {
+        console.log("I am mounted");
+    }
+
     render() {
         let linkColor = this.props.color;
+        let visitedColor = this.state.visited ? 'red': 'green';
         const linkStyle = {
-            color:linkColor,
+            color:visitedColor,
             fontWeight: 700,
             fontSize: "40px"
         };
         return (
-            <a href={this.props.apiUrl} style={linkStyle}>{this.props.urlName} </a>
+            <a onPointerMove={this.gotPointer}onClick={this.gotClicked}href={this.props.apiUrl} style={linkStyle}>{this.props.urlName} </a>
         );
     }
 }
 LinkComponenent.propTypes = {
-    urlName: PropTypes.number,
+    urlName: PropTypes.string,
     apiUrl: PropTypes.string,
     color: PropTypes.string
 }
